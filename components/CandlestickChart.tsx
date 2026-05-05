d80d139a039576f10de80c42bfbca9fd10668d45
DavitHarutyunyan98 <davitharutyunyan998@gmail.com>
2026-05-05 15:17:35 +0400

    fix: import Kline from lib/types instead of OHLCVDashboard

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import type { Kline } from "./OHLCVDashboard";
+import type { Kline } from "@/lib/types";
 
 interface Props {
   klines: Kline[];
